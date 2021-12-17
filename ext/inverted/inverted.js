303a7495c63c4eadc5126893a66f08cc265b22f8
Tom Demagny <tom.demagny@laposte.net>
2021-12-17 09:23:54 +0100

    optimise inverted script

--- ext/inverted/inverted.js
+++ ext/inverted/inverted.js
@@ -28,73 +28,45 @@ function setup() {
 }
 
 function loop() {
-    console.log('loop')
-    ctx.clearRect(0, 0, W, H)
     for (let i = 0; i < W; i+=pixelSize) {
         for (let j = 0; j < H; j+=pixelSize) {
             pixelVal[i][j] = 0;
         }
     }
-    /*for (let i = 0; i < W; i+=pixelSize) {
-        for (let j = 0; j < H; j+=pixelSize) {
-            var nbCicleIn = 0
-            for (let k = 0; k < circle.length; k++) {
-                if (circle[k].size >= Math.sqrt((circle[k].x - i)*(circle[k].x - i) + (circle[k].y - j)*(circle[k].y - j))) {
-                    nbCicleIn++
-                }
-                //Math.sqrt((circle[k].x - i*4)*(circle[k].x - i) + (circle[k].y - j)*(circle[k].y - j))
-            }
-            if (nbCicleIn%2) {
-                
-                ctx.fillStyle = "#EEE"
-            }else{
-                ctx.fillStyle = "#222"
-            }
-            ctx.fillRect( i, j, pixelSize, pixelSize );
-        }
-    }*/
 
-    for (let k = 0; k < circle.length; k++) {
-        for (let i = 0; i < circle[k].size*2; i+=1) {
-            for (let j = 0; j < circle[k].size*2; j+=1) {
-                if ( circle[k].size >= Math.sqrt( pow((circle[k].x + circle[k].size) - (i+circle[k].x)) + pow((circle[k].y + circle[k].size) - (j+circle[k].y)) ) ) {
+    for (let k = 0; k < circle.length; k++) { //nb de circle
+        for (let i = 0; i < circle[k].size*2; i+=1) { // width
+            for (let j = 0; j < circle[k].size*2; j+=1) { //height
+                if ((circle[k].x-circle[k].size+i) >= 0 && (circle[k].y-circle[k].size+j) >= 0 && (circle[k].x-circle[k].size+i) < W && (circle[k].y-circle[k].size+j) < H ) { //si c'est dans l'ecran
+                    if ( circle[k].size >= Math.sqrt( pow((circle[k].x + circle[k].size) - (i+circle[k].x)) + pow((circle[k].y + circle[k].size) - (j+circle[k].y)) ) ) { //si c'est dans le cercle
 
-                    if ((circle[k].x-circle[k].size+i) >= 0 && (circle[k].y-circle[k].size+j) >= 0 && (circle[k].x-circle[k].size+i) < W && (circle[k].y-circle[k].size+j) < H ) {
-                        
                         pixelVal[circle[k].x-circle[k].size+i][circle[k].y-circle[k].size+j]++
                     }
                 }
             }            
         }
+        circle[k].update()
     }
     
     ctx.fillStyle = "#222"
+    ctx.fillRect( 0, 0, W, H ); // le fond
+    ctx.fillStyle = "#EEE"
     for (let i = 0; i < W; i+=pixelSize) {
         for (let j = 0; j < H; j+=pixelSize) {
             if (pixelVal[i][j] %2) {
                 
-                ctx.fillStyle = "#EEE"
-                ctx.fillRect( i, j, pixelSize, pixelSize );
-                ctx.fillStyle = "#222"
-            }else{
                 ctx.fillRect( i, j, pixelSize, pixelSize );
             }
         }
     }
 
-    for (let i = 0; i < circle.length; i++) {
-        circle[i].update()
-        /*ctx.beginPath()
-        ctx.arc(circle[i].x, circle[i].y, circle[i].size, 0, 2 * Math.PI)
-        ctx.stroke()*/
-    }
 }
 
 function Circle() {
     this.x = nb_random(0, W)
     this.y = nb_random(0, H)
     this.size = nb_random(50, 100)
-    this.direction = nb_random(0, 2 * 3.1415)
+    this.direction = nb_random(0, 200 * 3.1415)/100
     this.speed = 5
     this.update = function() {
         this.x += Math.cos(this.direction)*this.speed;
