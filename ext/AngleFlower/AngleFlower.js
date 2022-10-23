da6c7cf2771ba9c79935847100c5ae8600f38602
Tom Demagny <tom.demagny@laposte.net>
2022-10-23 21:04:21 +0200

    change

--- ext/AngleFlower/AngleFlower.js
+++ ext/AngleFlower/AngleFlower.js
@@ -8,13 +8,13 @@ var posY = 0;
 var zoom = 1;
 var angle = 0;
 var length = 3; //size of first seg
-var stepByFrame = 10000; //nb de seg
+var stepByFrame = 20000; //nb de seg
 
 var time = 1; //change on each frame
 let state = 1; //change for each little segment
 
 let waveCollection = []; //collection de fonction sin pour faire somme de fourier
-noise.seed(Math.random()); //init de perlin noise
+noise.seed(Math.random() * 100); //init de perlin noise
 
 function setup() {
     canvas = document.getElementById("angleFlower")
@@ -30,10 +30,11 @@ function setup() {
 
     ctx.fillStyle = "#111";
     ctx.fillRect(0, 0, W, H);
-    ctx.strokeStyle = "#FFF";
+    ctx.strokeStyle = "#EEE";
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 2; i++) {
         waveCollection.push(new wave());
+        waveCollection[i].init();
     }
 
     setInterval(loop, 10);
@@ -46,6 +47,7 @@ function loop() {
         waveCollection[i].update(0.1, 0.2, 0.3, 0.4); //4 param diff pour avoir des vals diff
     }
 
+    //position du début de la chaine
     let lastX = posX;
     let lastY = posY;
 
@@ -57,30 +59,36 @@ function loop() {
 
     for (let i = 0; i < stepByFrame; i++) {
 
-        ctx.beginPath();
-        ctx.moveTo(posX*zoom + (W / 2), posY*zoom + (H / 2));
-        posX += Math.cos((angle) * 3.1415926 / 180) * length * Math.exp(-state / 5000);
-        posY += Math.sin((angle) * 3.1415926 / 180) * length * Math.exp(-state / 5000);
-        ctx.lineTo(posX*zoom + (W / 2), posY*zoom + (H / 2));
-        ctx.stroke();
+        if (i % 3 == 0) { //pour faire de l'optimisation (on voit pas la diff)
+
+            ctx.beginPath();
+            ctx.moveTo(posX * zoom + (W / 2), posY * zoom + (H / 2));
+            posX += Math.cos((angle) * 3.1415926 / 180) * length * Math.exp(-state / 5000);
+            posY += Math.sin((angle) * 3.1416926 / 180) * length * Math.exp(-state / 5000);
+            ctx.lineTo(posX * zoom + (W / 2), posY * zoom + (H / 2));
+            ctx.stroke();
+        }
 
         angle += AngleAdder(state);
 
-        if(i%50 == 0){
+        if (i % 50 == 0) {
             moyenneX += posX;
             moyenneY += posY;
-            moyenneSize += Math.sqrt(posX*posX + posY*posY);
+            moyenneSize += Math.sqrt(posX * posX + posY * posY);
             moyenneCount++;
         }
 
         state++;
     }
 
-    zoom = ((H/2)/(moyenneSize/moyenneCount))/5;
-    posX = (lastX - moyenneX/moyenneCount);//pour centrer la figure
-    posY = (lastY - moyenneY/moyenneCount);
+    deltaX = posX;
+    deltaY = posY;
+
     angle = 0;
-    time++;
+    zoom = ((H / 2) / (moyenneSize / moyenneCount)) / 5;
+    posX = (lastX - moyenneX / moyenneCount); //pour centrer la figure
+    posY = (lastY - moyenneY / moyenneCount);
+    time += 1 
     state = 0;
 
 }
@@ -88,7 +96,7 @@ function loop() {
 
 
 function AngleAdder(s) {
-    let step = s / 50
+    let step = s / 100
     let a = 0;
     for (let i = 0; i < waveCollection.length; i++) {
         a += waveCollection[i].get(step);
@@ -105,15 +113,31 @@ function wave() {
     this.c = 1;
     this.d = 1;
 
-    this.get = function(x) {
-        return Math.sin(x * this.a*0.1 + this.b) * this.c *1.5 + this.d / 1.5;
+    this.aP ;
+    this.bP ;
+    this.cP ;
+    this.dP ;
+
+    this.init = function() {
+        this.aP = Math.random();
+        this.bP = Math.random();
+        this.cP = Math.random();
+        this.dP = Math.random();
     }
 
-    this.update = function(a = 1, b = 1, c = 1, d = 1) {
-        this.a = noise.perlin2(a, time / 7000 + 0.001) * 10;
-        this.b = noise.perlin2(b, time / 7000 + 0.001) * 10;
-        this.c = noise.perlin2(c, time / 70000 + 0.001) * 5;
-        this.d = noise.perlin2(d, time / 7000 + 0.001) * 10;
+    this.get = function(x) {
+        return Math.sin(x * this.a * 0.1 + this.b) * this.c * 1.5 + this.d / 1.5;
+    }
+
+    this.update = function() {
+        this.a = noise.perlin2(this.aP, (time / 7000) + 0.001) * 10;
+        this.b = noise.perlin2(this.bP, (time / 7000) + 0.001) * 10;
+        this.c = noise.perlin2(this.cP, (time / 7000) + 0.001) * 10;
+        this.d = noise.perlin2(this.dP, (time / 7000) + 0.001) * 10;
+        /*this.a = Math.cos(this.aP + time / 500) * 1;
+        this.b = Math.cos(this.bP + time / 500) * 1;
+        this.c = Math.cos(this.cP + time / 500) * 1;
+        this.d = Math.cos(this.dP + time / 500) * 1;*/
     }
 }
 
